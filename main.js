a1ede0e061b34f54219802835d37cfd0388db0d3
mncross <mncross@gmail.com>
2015-11-09 19:44:29 -0600

    began using ng-model to data bind

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ var app = angular.module('TodoApp', []);
 app.controller('TodoController', function($scope){
 	$scope.title = "Welcome to your todo app";
 });
+
+app.controller('pastaController', function($scope){
+	$scope.pasta = "I love carbs";
+	$scope.foods = '';
+});
