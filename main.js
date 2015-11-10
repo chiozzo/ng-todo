bf1a0f39f90ed187afba156192e8c634393ab0c7
mncross <mncross@gmail.com>
2015-11-09 20:34:01 -0600

    added todo list

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ var app = angular.module('TodoApp', []);
 
 app.controller('TodoController', function($scope){
 	$scope.title = "Welcome to your todo app";
+
+	$scope.todos = [
+		{name: "take out the garbage"},
+		{name: "feed the cat"},
+		{name: "give the dog medicine"}
+	];
 });
 
 app.controller('pastaController', function($scope){
